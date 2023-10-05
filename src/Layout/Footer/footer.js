522511fbdd7629658f720bc2166f85481e79f1f1
JThorneX <115962097+JThorneX@users.noreply.github.com>
2023-10-04 20:12:45 -0600

    mobile footer + more folders

--- src/Layout/Footer/footer.js
+++ src/Layout/Footer/footer.js
@@ -1,10 +1,15 @@
 import "./footer.scss";
 import Logo from "../../assets/logo/fcsw dark logo.svg";
+import { NavLink } from "react-router-dom";
+
+import { IoSettingsOutline, IoLogOutOutline } from "react-icons/io5";
+import { BsClipboardData } from "react-icons/bs";
+import { AiOutlineHome } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="header-content">
+      <div className="footerContent">
         <div className="logoInfo">
           <div className="logoMargin m-5 d-flex">
             <a href="cra-doc/" className="logo-section me-3 ">
@@ -59,6 +64,40 @@ const Footer = () => {
               {". "} All rights reserved
             </div>
           </div>
+        </div>
+      </div>
+      <div className="footerContentMobile">
+        <div className="footerContentMobileWrapper">
+          <section className="footerIconLinks">
+            <NavLink exact to="" className="footerHomeIcon footerMobileIconBox">
+              <AiOutlineHome className="footerMobileIcon" />
+              <h6>Home</h6>
+            </NavLink>
+            <NavLink
+              exact
+              to=""
+              className="footerMyChartIcon footerMobileIconBox"
+            >
+              <BsClipboardData className="footerMobileIcon" />
+              <h6>My Chart</h6>
+            </NavLink>
+            <NavLink
+              exact
+              to=""
+              className="footerSettingsIcon footerMobileIconBox"
+            >
+              <IoSettingsOutline className="footerMobileIcon" />
+              <h6>My Settings</h6>
+            </NavLink>
+            <NavLink
+              exact
+              to=""
+              className="footerLogoutIcon footerMobileIconBox"
+            >
+              <IoLogOutOutline className="footerMobileIcon" />
+              <h6>Logout</h6>
+            </NavLink>
+          </section>
         </div>
       </div>
     </div>
